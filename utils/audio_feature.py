ff8eeeb45274f27b4cd5cb267b2899440398beea
HCIMaker <First time log in>
2020-09-20 00:22:36 +0800

    Finish librosa part of extracting MFCC/log_mel

--- utils/audio_feature.py
+++ utils/audio_feature.py
@@ -15,6 +15,36 @@ import matplotlib.pyplot as plt
 print(librosa.__version__) #0.6.0
 print(np.__version__) #1.13.1
 print(matplotlib.__version__)#3.1.0
-#Test here
+
+def fileloader(filepath):
+    '''
+    Load the Video file and print the sr,duration and length of audio sequence
+    '''
+    y,sr = librosa.load(filepath)
+    drtime = round(librosa.get_duration(y,sr=sr)/60,3)
+    y_size = y.size
+
+    print("Sample Rate = {sr}\n".format(sr = sr))
+    print("Duration = {drtime} min\n".format(drtime = drtime))
+    print("Size of audio seqence = {y_size}\n".format(y_size = y_size))
+    return y,sr
+
+def log_mel(y,sr,n_fft=2048,hop_length=512,n_mels=80):
+    '''
+    Extract the log mel features by utilizing this function
+    '''
+    mel_spec = librosa.feature.melspectrogram(y, sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+    log_mel = librosa.amplitude_to_db(mel_spec)
+    return log_mel
+
+def pre_emph(y):
+    return pre_emph_y = y[1:]-0.97*y[:-1]
+
+def mfcc(y,sr,n_mfcc=13):
+    return mfcc = librosa.feature.mfcc(y=pre_emph, sr=sr, n_mfcc=n_mfcc)
+
+
+
+
 
 
