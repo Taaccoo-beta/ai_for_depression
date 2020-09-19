57c27e8f8134195cfcda90ebb86166f8cac627c1
Tongcoco <First time log in>
2020-09-19 23:25:09 +0800

    import module

--- utils/audio_feature.py
+++ utils/audio_feature.py
@@ -4,4 +4,16 @@
 # Written by 
 # --------------------------------------------------------
 
+#In this document,there are two kinds of method of extracting
+#MFCC:1. By adopting librosa 2. By adopting Pytorch
+
+import librosa
+from librosa import display
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+print(librosa.__version__) #0.6.0
+print(np.__version__) #1.13.1
+print(matplotlib.__version__)#3.1.0
+
 
